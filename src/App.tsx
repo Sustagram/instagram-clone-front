073c35d543c16f34ce02a363bcc685c7e379f95e
ArKc1009 <arkc1009@gmail.com>
2020-09-13 08:33:17 +0900

    feat : Register Component Front & Design,,?

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import "./App.css";
-import styled from "styled-components";
+import Register from './Page/Register';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Register></Register>
+  </div>;
 }
 
 export default App;
