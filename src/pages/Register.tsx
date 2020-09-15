8c14389e646482d95916e4dc0c8a712229e959b2
combbm <combbm@naver.com>
2020-09-15 23:58:53 +0900

    fix: apply react hooks at input forms

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,23 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import RegisterHeader from '../components/Register/RegisterHeader';
-
-const Container = styled.div`
-  width: 350px;
-  margin: 0 auto;
-  border: 1px solid rgb(222, 222, 222);
-  text-align: center;
-
-  &:first-child {
-    margin-top: 80px;
-    padding: 10px 10px 30px 10px;
-  }
-
-  &:last-child {
-    margin-top: 10px;
-    padding: 30px 10px 30px 10px;
-  }
-`;
+import RegisterBox from '../components/Register/RegisterBox';
+import RegisterFooter from '../components/Register/RegisterFooter';
+import RegisterTip from '../components/Register/RegisterTip';
 
 const Form = styled.form`
   display: flex;
@@ -44,66 +30,91 @@ const SubmitButton = styled.button`
   padding: 10px;
   margin-top: 10px;
   width: 77%;
+
   &:hover {
     cursor: pointer;
   }
 `;
 
-const Tip = styled.p`
-  font-size: 13px;
-  padding: 0 30px 0 30px;
-  color: rgb(160, 160, 160);
-`;
-
-const TipLink = styled.a`
-  text-decoration: none;
-  color: rgb(140, 140, 140);
-  font-weight: bold;
-`;
-
-const AcountWhether = styled.p`
-  margin: 0;
-  padding: 0;
-  font-size: 14px;
-`;
-
-const LoginLink = styled.a`
-  text-decoration: none;
-  color: #0095f6;
-`;
+interface RegisterState {
+  readonly email: string;
+  readonly realName: string;
+  readonly username: string;
+  readonly password: string;
+  readonly passwordConfirm: string;
+}
 
 const Register: React.FC = () => {
+  const [input, setInput] = useState<RegisterState>({
+    email: '',
+    realName: '',
+    username: '',
+    password: '',
+    passwordConfirm: ''
+  });
+
+  const onInputChangeHandle = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: keyof RegisterState
+  ) => {
+    e.persist();
+
+    setInput((current) => {
+      return {
+        ...current,
+        [type]: e.target.value
+      };
+    });
+  };
+
   return (
     <>
-      <Container>
+      <RegisterBox>
         <RegisterHeader />
 
         <Form action="">
-          <Input type="email" name="email" id="email" placeholder="이메일 주소" required />
-          <Input type="text" name="realname" id="realname" placeholder="성명" required />
-          <Input type="text" name="username" id="username" placeholder="사용자 이름" required />
-          <Input type="password" name="password" id="password" placeholder="비밀번호" required />
+          <Input
+            type="email"
+            placeholder="이메일 주소"
+            value={input.email}
+            onChange={(e) => onInputChangeHandle(e, 'email')}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="성명"
+            value={input.realName}
+            onChange={(e) => onInputChangeHandle(e, 'realName')}
+            required
+          />
+          <Input
+            type="text"
+            placeholder="사용자 이름"
+            value={input.username}
+            onChange={(e) => onInputChangeHandle(e, 'username')}
+            required
+          />
           <Input
             type="password"
-            name="passwordConfirm"
-            id="passwordConfirm"
+            placeholder="비밀번호"
+            value={input.password}
+            onChange={(e) => onInputChangeHandle(e, 'password')}
+            required
+          />
+          <Input
+            type="password"
             placeholder="비밀번호 확인"
+            value={input.passwordConfirm}
+            onChange={(e) => onInputChangeHandle(e, 'passwordConfirm')}
             required
           />
 
           <SubmitButton type="button">가입</SubmitButton>
         </Form>
-        <Tip>
-          가입하면 Sustagram의 <TipLink href="#">약관</TipLink>,{' '}
-          <TipLink href="#">데이터 정책</TipLink> 및 <TipLink href="#">쿠키 정책</TipLink>에
-          동의하게 됩니다.
-        </Tip>
-      </Container>
-      <Container>
-        <AcountWhether>
-          계정이 있으신가요? <LoginLink href="#">로그인</LoginLink>
-        </AcountWhether>
-      </Container>
+
+        <RegisterTip />
+      </RegisterBox>
+      <RegisterFooter />
     </>
   );
 };
