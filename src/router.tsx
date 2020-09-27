f6974ad2aece3a63b46993a8cec074b0953d2ce7
combbm <combbm@naver.com>
2020-09-27 14:02:38 +0900

    feat: apply MeProvider

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import { MeProvider } from './hooks/useMe';
 
 const Router: React.FC = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </Switch>
-  </BrowserRouter>
+  <MeProvider>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  </MeProvider>
 );
 
 export default Router;
