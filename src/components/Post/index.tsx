1bbe922b2d98d127ef5b7622eef91b58b8dd2c8f
ArKc1009 <arkc1009@gmail.com>
2020-10-18 17:59:36 +0900

    refactor: create atomic file to refactoring OptionButton component
    Post의 메뉴더보기 버튼 refactoring

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Test from '../../pages/test.png';
 import Card from '../Card';
+import OptionButton from '../../atomics/Button/OptionButton';
 import AttributeContainer from '../Attribute';
 import LikeCount from '../LikeCount';
 
@@ -28,25 +29,6 @@ const Profile = styled.div`
 const PosterNameDiv = styled.div`
   width: 100%;
   margin-left: 14px;
-`;
-
-const PostMenu = styled.div`
-  position: absolute;
-  top: 0;
-  right: 4px;
-  width: 40px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const PostMenuButton = styled.button`
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
 `;
 
 const ImageBox = styled.div`
@@ -179,15 +161,8 @@ const Post: React.FC = () => {
           <span className="poster">arkc</span>
         </PosterNameDiv>
       </Header>
-      <PostMenu>
-        <PostMenuButton type="button">
-          <svg aria-label="옵션 더 보기" fill="#262626" height="16" width="16" viewBox="0 0 48 48">
-            <circle clipRule="evenodd" cx="8" cy="24" fillRule="evenodd" r="4.5" />
-            <circle clipRule="evenodd" cx="24" cy="24" fillRule="evenodd" r="4.5" />
-            <circle clipRule="evenodd" cx="40" cy="24" fillRule="evenodd" r="4.5" />
-          </svg>
-        </PostMenuButton>
-      </PostMenu>
+
+      <OptionButton />
 
       <ImageBox>
         <ImageItem src={Test}></ImageItem>
