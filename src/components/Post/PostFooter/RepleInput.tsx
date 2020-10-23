89ba90ef3791968a74b02f0c1cc8fa1098d5643f
ArKc1009 <arkc1009@gmail.com>
2020-10-23 16:59:12 +0900

    feat: import ReactHooks

--- src/components/Post/PostFooter/RepleInput.tsx
+++ src/components/Post/PostFooter/RepleInput.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import UploadButton from '../../../atomics/Button/UploadButton';
