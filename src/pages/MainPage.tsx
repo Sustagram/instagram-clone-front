cc1466a4567821aaef1449f5ac9107ee5ce354d6
arkc <arkc1009@gmail.com>
2020-11-06 10:45:21 +0900

    chore: 팔로워 부분 구분위한 style 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -34,13 +34,18 @@ const StoryAndPost = styled.div`
 
 const Follower = styled.div<{ positionLeft: number }>`
   position: fixed;
+  background-color: #fff;
+  width: 300px;
+  border: 1px solid rgb(222, 222, 222);
+  text-align: center;
+  line-height: 150px;
   height: 100vh;
   padding: 0;
   left: ${(props) => `${1126 - (1920 - props.positionLeft) * 0.5}px`};
   @media only screen and (max-width: 1000px) {
     display: none;
   }
-  top: 88px;
+  top: 60px;
 `;
 
 const MainPage: React.FC = () => {
