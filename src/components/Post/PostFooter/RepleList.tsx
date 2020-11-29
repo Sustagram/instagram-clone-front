ddcd88d35e9b559eea52937bc0ae95de283e830c
ArKc1009 <arkc1009@gmail.com>
2020-11-29 22:09:10 +0900

    feat: 댓글 프리뷰 문구 기능 구현
    - 댓글 3개 이상있을 때 모두보기 표시
    - 모두보기 X 일때는 댓글 2개만
    - 모두보기 O 일때는 댓글 리스트로

--- src/components/Post/PostFooter/RepleList.tsx
+++ src/components/Post/PostFooter/RepleList.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Reple from '../../Reple';
 
-const StyledRepleList = styled.div``;
+const StyledRepleList = styled.div`
+  margin-top: 4px;
+`;
 
-const RepleAllButton = styled.div`
-  margin-bottom: 4px;
-
+const RepleAllButton = styled.button`
+  border: none;
+  background-color: #fff;
+  margin: 0 0 4px 0;
+  padding: 0;
+  cursor: pointer;
+  outline: none;
   & * {
     text-decoration: none;
     color: rgb(142, 142, 142);
@@ -31,20 +37,42 @@ interface RepleListState {
 }
 
 const RepleList: React.FC<RepleListProps> = ({ list }) => {
-  return (
-    <StyledRepleList>
-      <RepleAllButton>
-        <a href="/">
-          댓글 <span>100</span>개 미리보기
-        </a>
-      </RepleAllButton>
+  const [isViewAll, setViewAll] = useState(false);
 
-      {list.map((item) => (
-        <Reple key={item.reply_id} writer={item.username}>
-          {item.text}
-        </Reple>
-      ))}
-    </StyledRepleList>
+  const onClickAllButton = () => {
+    setViewAll(!isViewAll);
+  };
+
+  const repleAllList = list.map((item) => {
+    return (
+      <Reple key={item.reply_id} writer={item.username}>
+        {item.text}
+      </Reple>
+    );
+  });
+
+  const repleList = list.slice(0, 2).map((item) => {
+    return (
+      <Reple key={item.reply_id} writer={item.username}>
+        {item.text}
+      </Reple>
+    );
+  });
+
+  const buttonText = isViewAll ? (
+    <div>댓글 간단히 보기</div>
+  ) : (
+    <div>
+      댓글 <span>{list.length}</span>개 모두보기
+    </div>
+  );
+
+  return (
+    <div>
+      {list.length > 2 && <RepleAllButton onClick={onClickAllButton}>{buttonText}</RepleAllButton>}
+
+      <StyledRepleList>{isViewAll ? repleAllList : repleList}</StyledRepleList>
+    </div>
   );
 };
 
